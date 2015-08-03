bf373be7a28c838180c0b1be825c82e263439b52
jhnnslmk <mail@johanneslemke.com>
2015-08-04 00:43:44 +0200

    imagemagick init

---
+++ plugins/ImageMagick/ImageMagickManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VVVV.ImageMagick
+{
+    public class ImageMagickManager
+    {
+        public static ImageMagickManager Instance = new ImageMagickManager();
+        public string Directory;
+
+        public ImageMagickManager()
+        {
+        }
+    }
+}
+
